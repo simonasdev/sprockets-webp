dbe81c65e22b388687584a4d62d4962dcf60d111
Simonas Gildutis <simonasgildutis@gmail.com>
2020-11-19 17:19:35 +0200

    Make encoder configurable

--- lib/sprockets-webp.rb
+++ lib/sprockets-webp.rb
@@ -11,10 +11,14 @@ module Sprockets
     # Sprockets::WebP.encode_options = { quality: 100, lossless: 1, method: 6, alpha_filtering: 2, alpha_compression: 0, alpha_quality: 100 }
     #
     class << self
-      attr_writer :encode_options
+      attr_writer :encode_options, :encoder
 
       def encode_options
         @encode_options ||= { quality: 100, lossless: 1, method: 6, alpha_filtering: 2, alpha_compression: 0, alpha_quality: 100 }
+      end
+      
+      def encoder
+        @encoder ||= ::WebP
       end
     end
   end
